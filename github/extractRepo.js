273443839db2ff74a818ba19c178b310ee8f45c8
Gil Moskowitz <gilmoskowitz@acm.org>
2015-11-18 11:13:12 -0500

    simplify repo detection by processing cmd line

--- github/extractRepo.js
+++ github/extractRepo.js
@@ -1,33 +1,14 @@
-var repo  = require('./github.1'),
-    repo2 = require('./github.2'),
-    summary = [];
+var fs = require('fs');
 
-//console.log(repo.length + ' ' + repo2.length);
-//console.log('===========================')
-
-repo = repo.concat(repo2);
-
-//console.log(repo.length);
-//console.log('===========================')
-
-repo.forEach(function (e) {
-//  console.log(e.name);
-  summary.push({ name: e.name,
-                 private: e.private,
-                 url:  e.html_url,
-                 desc: e.description });
-});
-//console.log('===========================')
-
-summary = summary.sort(function (a, b) {
-//  console.log(a.name + ' ' + b.name);
-  if (a.name < b.name)      return -1;
-  else if (a.name > b.name) return 1;
-  else                      return 0;
-});
-
-//console.log('===========================')
-summary.forEach(function (e) {
-  var ary = [e.name, e.private, e.url, e.desc]
-  console.log(ary.join('\t'));
+process.argv.forEach(function(val, index, ary) {
+  if (index < 2)
+    return;
+  var buffer = fs.readFileSync(val, 'utf8')
+    , data   = JSON.parse(buffer)
+    ;
+  
+  data.forEach(function (val, index, ary) {
+    var fields = [ val.name, val.private, val.html_url, val.description ];
+    console.log(fields.join('\t'));
+  });
 });
--- github/extractRepo.sh
+++ github/extractRepo.sh
@@ -6,15 +6,15 @@ DESTDIR="$TMPDIR/$PROG"
 DONE=false
 
 [ -d "$DESTDIR" ] || mkdir -p "$DESTDIR" || exit 1
-
+rm "$DESTDIR"/repos.*.json 
 I=1
 while ! $DONE ; do
-  DEST="$DESTDIR/github.${I}.json"
-  curl -u gilmoskowitz "https://api.github.com/orgs/xtuple/repos?page=${I}&per_page=100" > $DEST
+  DEST="$DESTDIR/repos.${I}.json"
+  curl -u gilmoskowitz${PWSUFFIX} "https://api.github.com/orgs/xtuple/repos?page=${I}&per_page=100" > $DEST
   if [ ! -e $DEST ] ; then DONE=true
   elif [ $(wc -l $DEST | awk '{print $1}') -lt 100 ] ; then DONE=true
   fi
   I=$(($I + 1))
 done
 
-cat "$DESTDIR"/github.*.json && rm "$DESTDIR"/github.*.json 
+node extractRepo.js "$DESTDIR"/repos.*.json
